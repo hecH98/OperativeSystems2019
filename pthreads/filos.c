c752f0df7f11f2886ab408967ce82fd16da69d4d
Hector Herrera <deux.assassin98@gmail.com>
2019-10-15 02:11:09 +0000

    problema spaguetti

---
+++ pthreads/filos.c
@@ -0,0 +1,66 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#define NUM_FILOS 5
+
+pthread_mutex_t forks[NUM_FILOS];
+
+void actions(){
+    sleep(1 + rand()%5);
+}
+
+void * life(void * ptId){
+    long tid;
+    tid = (long)ptId;
+    srand(time(NULL) + tid);
+    while(1){
+        printf("Voy a pensar un rato %ld\n", tid);
+        actions();
+        printf("Tengo hambre %ld\n", tid);
+        if(tid %2 == 0){
+            pthread_mutex_lock(&forks[tid]);
+            printf("Ya tengo el tenedor derecho %ld\n", tid);
+            pthread_mutex_lock(&forks[(tid+1%NUM_FILOS)]);
+            printf("Ya tengo el tenedor izquierdo %ld\n", tid);
+            printf("A comer! %ld\n", tid);
+            actions();
+            printf("Regresando tenedores %ld\n", tid);
+            pthread_mutex_unlock(&forks[tid]);
+            printf("Solte el tenedor derecho %ld\n", tid);
+            pthread_mutex_unlock(&forks[(tid+1%NUM_FILOS)]);
+            printf("Solte el tenedor izquierdo %ld\n", tid);
+
+        }else{
+            pthread_mutex_lock(&forks[(tid+1%NUM_FILOS)]);
+            printf("Ya tengo el tenedor izquierdo %ld\n", tid);
+            pthread_mutex_lock(&forks[tid]);
+            printf("Ya tengo el tenedor derecho %ld\n", tid);
+            printf("A comer! %ld\n", tid);
+            actions();
+            printf("Regresando tenedores %ld\n", tid);
+            pthread_mutex_unlock(&forks[(tid+1%NUM_FILOS)]);
+            printf("Solte el tenedor izquierdo %ld\n", tid);
+            pthread_mutex_unlock(&forks[tid]);
+            printf("Solte el tenedor derecho %ld\n", tid);
+        }
+    }
+
+}
+
+int main(int argc, char *argv[])
+{
+   pthread_t threads[NUM_FILOS];
+   int rc;
+   long t;
+   for(t=0;t<NUM_FILOS;t++){
+     printf("In main: creating thread %ld\n", t);
+     rc = pthread_create(&threads[t], NULL, life, (void *)t);
+     if (rc){
+       printf("ERROR; return code from pthread_create() is %d\n", rc);
+       exit(-1);
+       }
+    }
+    pthread_exit(NULL);
+}
